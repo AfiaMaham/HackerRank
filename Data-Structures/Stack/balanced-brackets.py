4b142748ec665817baa629d2b1695f2fcc691d94
AfiaMaham <afiamaham08@gmail.com>
2025-11-03 21:52:51 +0500

    balance brackets

---
+++ Data-Structures/Stack/balanced-brackets.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'isBalanced' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isBalanced(s):
+    stack = []
+    pairs = {')': '(', '}': '{', ']': '['}
+
+    for ch in s:
+        if ch in '({[':
+            stack.append(ch)
+        elif ch in ')}]':
+            if not stack or stack[-1] != pairs[ch]:
+                return "NO"
+            stack.pop()
+
+    return "YES" if not stack else "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
